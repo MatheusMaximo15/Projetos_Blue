55be258c4b47064569c707dae8efe4c6e0fe5789
Matheus Máximo Linhares Maciel Silva <74806715+MatheusMaximo15@users.noreply.github.com>
2021-08-23 19:39:29 -0300

    Update Projeto Detetive.js

--- Projeto Detetive.js
+++ Projeto Detetive.js
@@ -7,106 +7,99 @@
 */
 
 console.log("Jogo do detetive");
-console.log(`Você é suspeito de um crime! Responda as perguntas do detetive. Suas respostas determinarão se você é ou não o culpado pelo crime! Boa sorte! :D`);
+console.log(
+  `Você é suspeito de um crime! Responda as perguntas do detetive. Suas respostas determinarão se você é ou não o culpado pelo crime! Boa sorte! :D`
+);
 
-
-let contador = 0
+let contador = 0;
 
 let resposta = prompt("Você telefonou para a vítima?\n").toLowerCase();
 
-while (resposta != "s" && resposta != "n" && resposta != "sim" && resposta != "não" && resposta != "nao") {
+while (
+  resposta != "s" &&
+  resposta != "n" &&
+  resposta != "sim" &&
+  resposta != "não" &&
+  resposta != "nao"
+) {
   resposta = prompt(`
     Responda somente com s ou n.
-    \nVocê telefonou para a vítima?\n`).toLowerCase()
+    \nVocê telefonou para a vítima?\n`).toLowerCase();
 }
 if (resposta == "s" || resposta == "sim") {
-  ++contador ;
+  ++contador;
 }
- resposta = prompt("\nVocê esteve no local do crime?\n").toLowerCase();
- 
- while (resposta != "s" && resposta != "n" && resposta != "sim" && resposta != "não" && resposta != "nao") {
-   resposta = prompt(`
+resposta = prompt("\nVocê esteve no local do crime?\n").toLowerCase();
+
+while (
+  resposta != "s" &&
+  resposta != "n" &&
+  resposta != "sim" &&
+  resposta != "não" &&
+  resposta != "nao"
+) {
+  resposta = prompt(`
    Responda somente com s ou n.
-   \nVocê esteve no local do crime?\n`).toLowerCase()
- }
+   \nVocê esteve no local do crime?\n`).toLowerCase();
+}
 if (resposta == "s" || resposta == "sim") {
   ++contador;
 }
- resposta = prompt("\nVocê mora perto da vítima?\n").toLowerCase();
- 
- while (resposta != "s" && resposta != "n" && resposta != "sim" && resposta != "não" && resposta != "nao") {
+resposta = prompt("\nVocê mora perto da vítima?\n").toLowerCase();
+
+while (
+  resposta != "s" &&
+  resposta != "n" &&
+  resposta != "sim" &&
+  resposta != "não" &&
+  resposta != "nao"
+) {
   resposta = prompt(`
     Responda somente com s ou n.
-    \nVocê mora perto da vítima?\n`).toLowerCase()
-  }
+    \nVocê mora perto da vítima?\n`).toLowerCase();
+}
 if (resposta == "s" || resposta == "sim") {
   ++contador;
 }
- resposta = prompt("\nVocê devia para a vítima?\n").toLowerCase();
-  
-  while (resposta != "s" && resposta != "n" && resposta != "sim" && resposta != "não" && resposta != "nao") {
+resposta = prompt("\nVocê devia para a vítima?\n").toLowerCase();
+
+while (
+  resposta != "s" &&
+  resposta != "n" &&
+  resposta != "sim" &&
+  resposta != "não" &&
+  resposta != "nao"
+) {
   resposta = prompt(`
     Responda somente com s ou n.
-    \nVocê devia para a vítima?\n`).toLowerCase()
-  }
+    \nVocê devia para a vítima?\n`).toLowerCase();
+}
 if (resposta == "s" || resposta == "sim") {
   ++contador;
 }
- resposta = prompt("\nVocê já trabalhou com a vítima?\n").toLowerCase();
- 
- while (resposta != "s" && resposta != "n" && resposta != "sim" && resposta != "não" && resposta != "nao") {
+resposta = prompt("\nVocê já trabalhou com a vítima?\n").toLowerCase();
+
+while (
+  resposta != "s" &&
+  resposta != "n" &&
+  resposta != "sim" &&
+  resposta != "não" &&
+  resposta != "nao"
+) {
   resposta = prompt(`
     Responda somente com s ou n.
-    \nVocê já trabalhou com a vítima?\n`).toLowerCase()
-  }
+    \nVocê já trabalhou com a vítima?\n`).toLowerCase();
+}
 if (resposta == "s" || resposta == "sim") {
   ++contador;
-} 
- 
+}
 
- if (contador < 2) {
-   console.log(`Você é inocente! \nFez ${contador} ponto(s)!`)
- } else if (contador == 2) {
-   console.log(`Você é suspeito! \nFez ${contador} ponto(s)!`)
- } else if (contador > 2 && contador < 5) {
-   console.log(`Você é cúmplice! \nFez ${contador} ponto(s)!`)
- } else if (contador == 5) {
-   console.log(`Você é o assassino! Não se mexa! \nFez ${contador} ponto(s)!`)
- }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//Por Matheus Máximo.
-  
-
-  
-  
+if (contador < 2) {
+  console.log(`Você é inocente! \nFez ${contador} ponto(s)!`);
+} else if (contador == 2) {
+  console.log(`Você é suspeito! \nFez ${contador} ponto(s)!`);
+} else if (contador > 2 && contador < 5) {
+  console.log(`Você é cúmplice! \nFez ${contador} ponto(s)!`);
+} else if (contador == 5) {
+  console.log(`Você é o assassino! Não se mexa! \nFez ${contador} ponto(s)!`);
+}
